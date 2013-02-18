607f7a8ae386d440ccdac59126c7f28fb88dd6ea
Faddah Wolf <my_biz@me.com>
2013-02-18 15:52:42 -0800

    The javascript and html files for this test project
    The javascript and html files for this test project

--- fader-text-script.js
+++ fader-text-script.js
@@ -1,32 +1,3 @@
-/*  
-function showRecommendation() {
-	var words = new Array();
-		words[0] = "“Your deepest presence is in your every small contracting and expanding. The two as beautifully balanced and coordinated as birdwings.” -Rumi";
-		words[1] = "“What is this precious love and laughter budding in our hearts? It is the glorious sound of a soul waking up.” ~Hafiz";
-		words[2] = "“No matter where you go... there you are!” -Buckaroo Banzai";
-		words[3] = "“Zaphod's... just this guy, you know?” -Douglas Adams' The Hitchhiker's Guide To The Galaxy";
-	/*  var words = ["“Your deepest presence is in your every small contracting and expanding. The two as beautifully balanced and coordinated as birdwings.” -Rumi", "“What is this precious love and laughter budding in our hearts? It is the glorious sound of a soul waking up.” ~Hafiz", "“No matter where you go... there you are!” -Buckaroo Banzai", "“Zaphod's... just this guy, you know?” -Douglas Adams' The Hitchhiker's Guide To The Galaxy"];
-	var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
-	var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];  */
-/*  
-	var rand = Math.floor(Math.random() * words.length);
-	/*  var rand2 = Math.floor(Math.random() * words2.length);
-	var rand3 = Math.floor(Math.random() * words3.length);  */
-/*  
-	var recommend = words[rand];  /* + " " + words2[rand2] + " " + words3[rand3]  */
-/*  
-	var recommendElement = document.getElementById("recommend");
-	recommendElement.innerHTML = recommend;
-}  */
-/*  window.onload = showRecommendation;  */
-/*  
-$(document).ready(function() {
-	
-		$('#recommend').showRecommendation
-		/*  .fadeIn('slow').delay(5000).fadeOut('slow');  */
-/*  
-});  */
-
 $(document).ready(function () {
     var words = new Array();
     words[0] = "“Your deepest presence is in your every small contracting and expanding. The two as beautifully balanced and coordinated as birdwings.” -Rumi";
